6dd8dba74b347e3248befe8d16a9c03b3e6849e7
lucasfrsi <lucasfrsi@gmail.com>
2022-03-27 10:56:28 -0300

    refactor(locales): add types to i18n

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -7,6 +7,7 @@ import enPokemon from './en/pokemon.json';
 import ptCommon from './pt/common.json';
 import ptPokemon from './pt/pokemon.json';
 
+export const defaultNS = 'common';
 export const resources = {
   en: {
     common: enCommon,
@@ -16,16 +17,16 @@ export const resources = {
     common: ptCommon,
     pokemon: ptPokemon,
   },
-};
+} as const;
 
 export const i18n = i18next
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources,
     fallbackLng: 'en',
     ns: ['common', 'pokemon'],
-    defaultNS: 'common',
+    resources,
+    defaultNS,
 
     debug:
       process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test',
